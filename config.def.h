a3e7bb07b9d51abd1ddc476586ffab689dd64dd7
Ryan Kes <alrayyes@gmail.com>
2019-03-29 10:59:09 +0100

    st[PATCH]: Disable italics

--- config.def.h
+++ config.def.h
@@ -6,6 +6,12 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Cascadia Code:pixelsize=18:antialias=true:autohint=true";
+
+/* disable bold, italic and roman fonts globally */
+int disablebold = 0;
+int disableitalic = 1;
+int disableroman = 0;
+
 static int borderpx = 2;
 
 /*
