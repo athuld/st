225425fd17cd8d2bf0d5bc5b2ab2840cea6b2828
Athul <athuldinesan005@gmail.com>
2020-11-29 21:29:01 +0530

    st: dracula color scheme

--- config.def.h
+++ config.def.h
@@ -98,47 +98,51 @@ float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"#313539", /* black   */
-	"#b02626", /* red     */
-	"#40a62f", /* green   */
-	"#f2e635", /* yellow  */
-	"#314ad0", /* blue    */
-	"#b30ad0", /* magenta */
-	"#32d0fc", /* cyan    */
-	"#acadb1", /* white   */
+  /* 8 normal colors */
+  [0] = "#000000", /* black   */
+  [1] = "#ff5555", /* red     */
+  [2] = "#50fa7b", /* green   */
+  [3] = "#f1fa8c", /* yellow  */
+  [4] = "#bd93f9", /* blue    */
+  [5] = "#ff79c6", /* magenta */
+  [6] = "#8be9fd", /* cyan    */
+  [7] = "#bbbbbb", /* white   */
 
-	/* 8 bright colors */
-	"#676f78", /* black   */
-	"#b55454", /* red     */
-	"#78a670", /* green   */
-	"#faf380", /* yellow  */
-	"#707fd0", /* blue    */
-	"#c583d0", /* magenta */
-	"#8adaf1", /* cyan    */
-	"#e0e3e7", /* white   */
+  /* 8 bright colors */
+  [8]  = "#44475a", /* black   */
+  [9]  = "#ff5555", /* red     */
+  [10] = "#50fa7b", /* green   */
+  [11] = "#f1fa8c", /* yellow  */
+  [12] = "#bd93f9", /* blue    */
+  [13] = "#ff79c6", /* magenta */
+  [14] = "#8be9fd", /* cyan    */
+  [15] = "#ffffff", /* white   */
 
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"black",
+  /* special colors */
+  [256] = "#282a36", /* background */
+  [257] = "#f8f8f2", /* foreground */
 };
 
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor 
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
 
 /*
+ * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ * will reverse too. Another logic would only make the simple feature too
+ * complex.
+ */
+unsigned int defaultitalic = 7;
+unsigned int defaultunderline = 7;
+/*
  * Default shape of cursor
- * 2: Block ("█")
+ * 2: Block ("|")
  * 4: Underline ("_")
  * 6: Bar ("|")
  * 7: Snowman ("☃")
