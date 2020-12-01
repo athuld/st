2c44e2d6764135d2043316bf3c594b4788aa8ac5
Athul <athuldinesan005@gmail.com>
2020-12-02 01:35:34 +0530

    st: anysize patch (fixes with wierd gaps)

--- config.def.h
+++ config.def.h
@@ -147,7 +147,7 @@ unsigned int defaultunderline = 7;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 6;
 
 /*
  * Default columns and rows numbers
