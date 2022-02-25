eaf292f079fa6f75d5d7b22fee2adef0143eed46
Athul Dinesan <athuldinesan5@gmail.com>
2022-02-26 01:32:26 +0530

    st[PATCH]: font2 patch

--- config.def.h
+++ config.def.h
@@ -5,7 +5,12 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Cascadia Code:pixelsize=18:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:pixelsize=18:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+"Inconsolata for Powerline:pixelsize=18:antialias=true:autohint=true",
+"Hack Nerd Font Mono:pixelsize=18:antialias=true:autohint=true",
+};
 
 /* disable bold, italic and roman fonts globally */
 int disablebold = 0;
